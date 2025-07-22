c11a04102589eca7b06b1ead4aa144b75eb3a3a9
Mayank Singh <mayank.singh01@nagarro.com>
2025-07-22 15:13:23 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -19,6 +19,7 @@ def call_recommendation_api(payload):
 
 def fetch_product_details_from_vtex(product_ids, fields="basic"):
     VTEX_BASE_URL = "https://dev.showcase-cashmere.store/api/ccs/product-details"
+
     logger.info("Fetching product details from VTEX for product IDs: %s", product_ids)
     def try_fetch(pid):
         params = {
@@ -31,9 +32,27 @@ def fetch_product_details_from_vtex(product_ids, fields="basic"):
 
             if response.status_code == 200:
                 product_json = response.json()
-                product = product_json.get("data", {}).get("product", {})
-                product_id = product.get("basic", {}).get("productId")
-                return product_id     
+                basic = product_json.get("data", {}).get("product", {}).get("basic", {})
+
+                default = basic.get("default", {})
+                default.pop("images", None)
+                default.pop("allImages", None)
+                default.pop("sellerId", None)
+                default.pop("listPrice", None)
+                
+                productinfo = product_json.get("data", {}).get("product", {}).get("basic", {})
+                productinfo.pop("productReference", None)
+                productinfo.pop("brandId", None)
+                productinfo.pop("titleTag", None)
+                productinfo.pop("metaTagDescription", None)
+                productinfo.pop("benefits", None) 
+                productinfo.pop("releaseDate", None)
+                productinfo.pop("productClusters", None)
+                productinfo.pop("link", None)
+                productinfo.pop("linkText", None)
+
+
+                return productinfo
             else:
                 logger.warning("VTEX API failed for ID %s with status %s", pid, response.status_code)
         except Exception as e:
